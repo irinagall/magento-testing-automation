ee7ccf4895bca0a4102d34f9fd7712397afd98c5
Howard <52459999+HowardC04@users.noreply.github.com>
2024-07-29 12:10:05 +0100

    Search Feature implementation (3.1.1)
    Finished the test for User story 3.1.
    Completed implementation for the Gherkin Scenario 3.1.1

--- src/test/java/webtestframework/pages/Website.java
+++ src/test/java/webtestframework/pages/Website.java
@@ -1,6 +1,8 @@
 package webtestframework.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Website {
 
@@ -22,5 +24,17 @@ public class Website {
 
     public String getPageTitle() {
         return webDriver.getTitle();
+    }
+
+    public WebDriver getWebDriver() {
+        return webDriver;
+    }
+
+    public WebElement getSearchBar(){
+        return webDriver.findElement(By.id("search"));
+    }
+
+    public WebElement getSearchButton(){
+        return webDriver.findElement(By.cssSelector("button[title='Search']"));
     }
 }
