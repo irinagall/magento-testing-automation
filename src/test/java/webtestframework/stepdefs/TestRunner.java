41ab43ffd533d982ea732b3c9de2cf8e18ddf4e1
Alastair Smith <alithton@proton.me>
2024-07-30 08:50:55 +0100

    Updated read me and added test reports to the repo

--- src/test/java/webtestframework/stepdefs/TestRunner.java
+++ src/test/java/webtestframework/stepdefs/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features"},
-        plugin = {"pretty", "html:target/testReport.html", "json:target/jsonReport.json"},
+        plugin = {"pretty", "html:reports/testReport.html", "json:reports/jsonReport.json"},
         publish = true
 )
 public class TestRunner {
